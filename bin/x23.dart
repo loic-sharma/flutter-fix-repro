e5e18450f1a4fd98525c841b3902c226d4b45d52
Loic Sharma <sharma.loic@gmail.com>
2023-04-20 15:03:38 -0700

    Add comment on desired state

--- bin/x23.dart
+++ bin/x23.dart
@@ -1,7 +1,13 @@
 import 'package:x23/services.dart';
 
 void main(List<String> arguments) {
+  // Original:
   ClipboardData();
   ClipboardData(text: null);
   ClipboardData(text: 'test');
+
+  // Desired state after dart fix:
+  // ClipboardData(text: '');
+  // ClipboardData(text: '');
+  // ClipboardData(text: 'test');
 }
